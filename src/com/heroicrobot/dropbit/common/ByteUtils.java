5a4752caf76f391b2295d58f7945835dadde98e2
Matthew Stone <matthew.d.stone@gmail.com>
2012-10-29 17:14:30 -0700

    Add bigEndian option

--- src/com/heroicrobot/dropbit/common/ByteUtils.java
+++ src/com/heroicrobot/dropbit/common/ByteUtils.java
@@ -36,13 +36,18 @@ public class ByteUtils {
     return i;
   }
 
-  public static final long byteArrayToLong(byte[] b) {
+  public static final long byteArrayToLong(byte[] b, boolean bigEndian) {
     if (b.length > 8) {
-      throw new IllegalArgumentException("The largest byte array that can fit in a long is 8");
+      throw new IllegalArgumentException(
+          "The largest byte array that can fit in a long is 8");
     }
     long value = 0;
     for (int i = 0; i < b.length; i++) {
-      value += ((long) b[i] & 0xFFL) << (8 * i);
+      if (bigEndian) {
+        value = (value << 8) + (b[i] & 0xFF);
+      } else {
+        value += ((long) b[i] & 0xFFL) << (8 * i);
+      }
     }
     return value;
   }

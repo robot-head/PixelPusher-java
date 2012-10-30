45da0a505db7bb5db32a851b7068d847b1c73c13
Matthew Stone <matt>
2012-10-30 13:54:46 -0700

    Adding PusherTask outline

---
+++ src/com/heroicrobot/dropbit/devices/pixelpusher/PusherTask.java
@@ -0,0 +1,21 @@
+package com.heroicrobot.dropbit.devices.pixelpusher;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.TimerTask;
+
+public class PusherTask extends TimerTask implements Observer {
+
+  @Override
+  public void update(Observable arg0, Object arg1) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void run() {
+    // TODO Auto-generated method stub
+
+  }
+
+}

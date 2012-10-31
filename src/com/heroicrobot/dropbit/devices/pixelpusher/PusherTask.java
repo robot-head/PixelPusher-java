a97595cfffad048221f5943f9e878cf7739e495f
Matthew Stone <matt>
2012-10-30 17:00:52 -0700

    Check in PusherTask changes in flight

--- src/com/heroicrobot/dropbit/devices/pixelpusher/PusherTask.java
+++ src/com/heroicrobot/dropbit/devices/pixelpusher/PusherTask.java
@@ -1,20 +1,48 @@
 package com.heroicrobot.dropbit.devices.pixelpusher;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.TimerTask;
 
+import com.heroicrobot.dropbit.registry.DeviceRegistry;
+
 public class PusherTask extends TimerTask implements Observer {
 
+  private Map<String, PixelPusher> pusherMap;
+
+  public PusherTask() {
+    this.pusherMap = new HashMap<String, PixelPusher>();
+  }
+  
   @Override
-  public void update(Observable arg0, Object arg1) {
-    // TODO Auto-generated method stub
+  public void update(Observable observable, Object update) {
+    DeviceRegistry registry = (DeviceRegistry) observable;
+    this.pusherMap = registry.getPusherMap();
 
   }
 
   @Override
   public void run() {
-    // TODO Auto-generated method stub
+    for (PixelPusher pusher : pusherMap.values()) {
+      int stripPerPacket = pusher.getMaxStripsPerPacket();
+      List<Strip> remainingStrips = pusher.getStrips();
+      List<Byte> packet;
+      while(!remainingStrips.isEmpty()) {
+        packet = new ArrayList<Byte>();
+        for(int i = 0; i < stripPerPacket; i++) {
+          if (remainingStrips.isEmpty()) {
+            break;
+          }
+          Strip strip = remainingStrips.remove(0);
+          List<Byte> stripBytes = new ArrayList<Byte>(Arrays.asList(strip.serialize()));
+        }
+      }
+    }
 
   }
 

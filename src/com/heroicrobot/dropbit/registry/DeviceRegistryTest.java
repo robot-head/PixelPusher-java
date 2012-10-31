b0743ca120028e14b29007ec4ff5f1fd85e40b7b
Matthew Stone <matthew.d.stone@gmail.com>
2012-10-30 21:55:16 -0700

    Make test blink random RGB values

--- src/com/heroicrobot/dropbit/registry/DeviceRegistryTest.java
+++ src/com/heroicrobot/dropbit/registry/DeviceRegistryTest.java
@@ -20,9 +20,17 @@ public class DeviceRegistryTest {
 
   private Random random = new Random();
   
-  public Pixel generateRandomPixel(int scaling) {
-    return new Pixel((byte)(random.nextInt(scaling)),(byte)(random.nextInt(scaling)),(byte)(random.nextInt(scaling)));
+  int[][] colors = {
+      {127, 0, 0},
+      {0, 127, 0},
+      {0, 0, 127}
+  };
+  
+  public Pixel generateRandomPixel() {
+    //return new Pixel((byte)(random.nextInt(scaling)),(byte)(random.nextInt(scaling)),(byte)(random.nextInt(scaling)));
     //return new Pixel((byte)(15), (byte)0, (byte)0);
+    int[] color = colors[random.nextInt(colors.length)];
+    return new Pixel((byte)color[0], (byte)color[1], (byte)color[2]);
   }
   
   @Test
@@ -45,17 +53,13 @@ public class DeviceRegistryTest {
     }
     TestObserver testObserver = new TestObserver();
     registry.addObserver(testObserver);
-    double scaling = 1d;
     while (true) {
       Thread.yield();
       if (testObserver.hasStrips) {
-        scaling += 0.001;
-        if (scaling > 255)
-          scaling = 1d;
         registry.startPushing();
         for(Strip strip : registry.getStrips()) {
           for (int i = 0; i < strip.getLength(); i++)
-            strip.setPixel(generateRandomPixel((int)(Math.round(scaling))), i);
+            strip.setPixel(generateRandomPixel(), i);
         }
       }
     }
